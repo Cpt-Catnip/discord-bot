ba5985dd3999851085d806fe8967275d7bed15b1
Mike Koshakow <mzkosh@gmail.com>
2021-01-19 21:26:02 -0500

    through vid 4

--- bot.js
+++ bot.js
@@ -1,4 +1,20 @@
+require('dotenv').config();
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.login();
+// create client
+const client = new Discord.Client();
+client.login(process.env.TOKEN);
+
+// even handlers
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', (msg) => {
+  const isTestChannel = msg.channel.id === process.env.TESTCHANNELID;
+
+  console.log(msg.content);
+  if (isTestChannel && msg.content === 'ping') {
+    msg.channel.send('pong');
+  }
+});
